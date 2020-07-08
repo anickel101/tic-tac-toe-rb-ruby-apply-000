9ad7e587905a2ed1bc11a0aae15c8ee155c14544
Anson Nickel <anson.nickel@gmail.com>
2020-07-08 20:58:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -142,14 +142,3 @@ def play(board)
   end
 end
 
-def play(board)
-  until over?(board) == true
-    turn(board)
-  end
-
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cats Game!"
-  end
-end
